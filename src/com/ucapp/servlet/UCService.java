23f06c477c7b2db892a9a5569db3ca023255b5b3
youlei <zha_zi@126.com>
2015-09-12 12:00:01 +0800

    xxx

--- src/com/ucapp/servlet/UCService.java
+++ src/com/ucapp/servlet/UCService.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.alibaba.fastjson.JSON;
-
-
 public class UCService extends HttpServlet {
 
 	/**
