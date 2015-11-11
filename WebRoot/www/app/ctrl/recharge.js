425965dfd4047096a2c9d9244cd7d2556f51641c
游雷 <lyou@Ctrip.com>
2015-11-11 15:36:21 +0800

    增加支付页面

--- WebRoot/www/app/ctrl/recharge.js
+++ WebRoot/www/app/ctrl/recharge.js
@@ -3,10 +3,63 @@ define(['jquery','underscore','backbone','text!TemplateRecharge','basePageView',
 	var loginView=basePageView.extend({
 	   events:{
 		   'click #gotoRecharge':"gotoRecharge", 
+		   "click #add":"add",
+		   "click #remove":"remove",
+		   "input #quantity":"inputNum",
+	   },
+	   
+	   inputNum:function(e){
+		   var $this=$(e.currentTarget);
+		   var self=this;
+		   var quantity=parseInt( self.$el.find("#quantity").val());
+		   var price=parseInt( self.$el.find("#price").data("price"));
+		   self.$el.find("#total").text(quantity*price+"元");
+		   self.$el.find("#total").data("total",quantity*price);
+	   },
+	   add:function(){
+		   var self=this;
+		   if($.isNumeric(self.$el.find("#quantity").val())){
+			   var quantity=parseInt( self.$el.find("#quantity").val()),
+			   	   price=parseInt( self.$el.find("#price").data("price"));
+			   quantity+=1;
+			   self.$el.find("#quantity").val(quantity);
+			   self.$el.find("#total").text(quantity*price+"元");
+			   self.$el.find("#total").data("total",quantity*price);
+		   }else{
+			   var quantity=parseInt( self.$el.find("#quantity").val()),
+		   	   	   price=parseInt( self.$el.find("#price").data("price"));
+			   self.showAlert("请输入数字");
+			   self.$el.find("#quantity").val("1");
+			   self.$el.find("#total").text(quantity*price+"元");
+			   self.$el.find("#total").data("total",quantity*price);
+		   }
+		   
 		   
 	   },
+	   remove:function(){
+		   var self=this;
+		   if($.isNumeric(self.$el.find("#quantity").val())){
+			   
+			   var quantity=parseInt( self.$el.find("#quantity").val()),
+			   	   price=parseInt( self.$el.find("#price").data("price"));
+			   if(quantity>1){
+				   quantity-=1;
+				   self.$el.find("#quantity").val(quantity);   
+				   self.$el.find("#total").text(quantity*price+"元");
+				   self.$el.find("#total").data("total",quantity*price);
+			   }
+			   
+		   }else{
+			   var quantity=parseInt( self.$el.find("#quantity").val()),
+	   	   	   price=parseInt( self.$el.find("#price").data("price"));
+			   self.showAlert("请输入数字");
+			   self.$el.find("#quantity").val("1");
+			   self.$el.find("#total").text(quantity*price+"元");
+			   self.$el.find("#total").data("total",quantity*price);
+		   }
+	   },
 	   gotoRecharge:function(){ 
-		   window.Native.gotoRecharge(localStorage.getItem("username"),$("#price").val());
+		   window.Native.gotoRecharge(localStorage.getItem("username"),$.trim($("#total").data("total")));
 	   },
 	   initTemplate: function () {
             return _.template(TemplateRecharge);
@@ -15,7 +68,7 @@ define(['jquery','underscore','backbone','text!TemplateRecharge','basePageView',
            var tpl = this.initTemplate(),
            	   self=this;  
        	   self.$el.html(tpl({ "data": data })); 
-       	
+       	   self.$el.find("#tel").text(localStorage.getItem("username"));
        },
        onCreate:function(){
         
